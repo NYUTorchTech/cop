5611c64a6e87f6d5c124830f6945622e66896ede
Jannae Jacks <jannae@gmail.com>
2015-03-20 15:46:07 -0400

    Many many issues with relative linking needed (still need) to be resolved.

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # NYU Communities of Practice
 
 The repository and current home for the NYU Communities of Practice website.
+
+## Contributing / Development
+
+Dependencies:
+* Ruby v2.2.0
+* Node.js v0.12.0
+* NPM v2.7.1
+
+To get started, clone this repository's `master` branch
+
+    git clone https://github.com/NYUTorchTech/cop.git
+
+Create a branch for your edits (never do work on `master`! :) ):
+
+    git checkout -b myname-myedits
+
+From the new `cop` directory, run:
+
+    bundle install
+    npm install
+    bower install
+
+Open in your favorite editor and code away!
+
+When you're ready,
+
+## Building and running the project with gulp
+
+Default task: Builds and serves the project locally. Watches for file updates.
+
+    gulp
+
+`publish` task: Builds the project for publishing to production, does not serve it.
+
+    gulp publish
+
+    # To serve a production build, follow the build command with:
+    gulp serve:prod
+    # [BUG] Images are broken when serving prod locally
+
+`deploy` task: Deploys the site built with `publish` to this repository's `gh-pages` branch
+
+    gulp deploy
+
+`rsync` task*: Deploys to a server (not github). Here as a placeholder for future plans.
+
+    gulp rsync
+
+*This task depends on a credentials file in your root directory.
+
+    cp rsync.json.example ._rsync.json
+
+Edit the new `._rsync.json` file to include the necessary information for an rsync deploy. **DO NOT COMMIT THIS FILE**
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-// Generated on 2015-02-05 using generator-jekyllized 0.7.1
+// Generated on 2015-03-20 using generator-jekyllized 0.7.1
 'use strict';
 
 var gulp = require('gulp');
 // Loads the plugins without having to list all of them, but you need
 // to call them as $.pluginname
 var $ = require('gulp-load-plugins')();
-// 'del' is used to clean out directories and such
+// "del" is used to clean out directories and such
 var del = require('del');
 // BrowserSync isn't a gulp package, and needs to be loaded manually
 var browserSync = require('browser-sync');
@@ -17,142 +17,119 @@ var reload = browserSync.reload;
 var bs;
 
 var php = require('gulp-connect-php');
+var wiredep = require('wiredep');
 
 // useful file paths
 var path = {
   src    : 'src',
   serve  : 'serve',
-  deploy : 'site',
+  site   : 'site',
   bower  : '_bower_components',
   assets : 'assets',
   css    : 'assets/css',
+  scss   : '_sass',
   js     : 'assets/js',
   img    : 'assets/img',
   fonts  : 'assets/fonts'
 };
 
-var wiredep = require('wiredep');
 
 // Deletes the directory that is used to serve the site during development
 gulp.task('clean:dev', del.bind(null, [path.serve]));
 
 // Deletes the directory that the optimized site is output to
-gulp.task('clean:prod', del.bind(null, [path.deploy]));
+gulp.task('clean:prod', del.bind(null, [path.site]));
 
 // Runs the build command for Jekyll to compile the site locally
 // This will build the site with the production settings
 gulp.task('jekyll:dev', $.shell.task('jekyll build'));
-gulp.task('jekyll-rebuild', ['jekyll:dev'], function () { reload; });
+gulp.task('jekyll-rebuild', ['jekyll:dev'], function () {
+  reload;
+});
 
 // Almost identical to the above task, but instead we load in the build configuration
 // that overwrites some of the settings in the regular configuration so that you
 // don't end up publishing your drafts or future posts
-gulp.task('jekyll:prod', $.shell.task('jekyll build --verbose --config _config.yml,_config.build.yml'));
+gulp.task('jekyll:prod', $.shell.task('jekyll build --config _config.yml,_config.build.yml'));
+
+// Compiles the SASS files and moves them into the "assets/css" directory
+gulp.task('styles', function () {
+  // Looks at the style.scss file for what to include and creates a style.css file
+  return gulp.src(path.src + '/' + path.scss + '/main.scss')
+    .pipe($.sass())
+    // AutoPrefix your CSS so it works between browsers
+    .pipe($.autoprefixer('last 1 version', { cascade: true }))
+    // Directory your CSS file goes to
+    .pipe(gulp.dest(path.src + '/' + path.css + '/'))
+    .pipe(gulp.dest(path.serve + '/' + path.css + '/'))
+    // Outputs the size of the CSS file
+    .pipe($.size({title: 'styles'}))
+    // Injects the CSS changes to your browser since Jekyll doesn't rebuild the CSS
+    .pipe(reload({stream: true}));
+});
 
 // Optimizes the images that exists
 gulp.task('images', function () {
-  return gulp.src(path.src + '/' + path.img +'/**')
-    .pipe($.changed(path.deploy + '/' + path.img))
+  // Look in the serve directory because that's where Jekyll puts the generated images
+  // Speaking of, why am I double optimizing??
+  // TODO: Fix this.
+  return gulp.src(path.serve + '/' + path.img + '/**/*.*')
+    .pipe($.changed(path.site + '/' + path.img + '/**/*.*'))
     .pipe($.imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
       interlaced: true
     }))
-    .pipe(gulp.dest(path.deploy + '/' + path.img))
+    .pipe(gulp.dest(path.site + '/' + path.img))
     .pipe($.size({title: 'images'}));
 });
 
-// Copy over fonts to the path.deploy directory
+// Copy over fonts to the "site" directory
 gulp.task('fonts', function () {
   return gulp.src(path.src + '/' + path.fonts + '/**')
-    .pipe(gulp.dest(path.deploy + '/' + path.fonts))
+    .pipe(gulp.dest(path.site + '/' + path.fonts))
     .pipe($.size({ title: 'fonts' }));
 });
 
-
-// Injecting our bower components and dependencies
-gulp.task('loaddeps', function() {
-
-  gulp.src(path.bower + '/font-awesome/fonts/**')
-    .pipe(gulp.dest(path.src + '/' + path.fonts));
-
-  gulp.src(wiredep().js).pipe(gulp.dest(path.src + '/' + path.js + '/vendor/'));
-  gulp.src(wiredep().css).pipe(gulp.dest(path.src + '/' + path.css  + '/vendor/'));
-
-  gulp.src(path.src + '/_layouts/default.html')
-    .pipe(wiredep.stream({
-      fileTypes: {
-        html: {
-          replace: {
-            js: function(filePath) {
-              return '<script src="/' + path.js + '/vendor/'+ filePath.split('/').pop() + '"></script>';
-            },
-            css: function(filePath) {
-              return '<link rel="stylesheet" href="' + path.css + '/vendor/' + filePath.split('/').pop() + '"/>';
-            }
-          }
-        }
-      }
-    }))
-
-    .pipe($.inject(
-      gulp.src([path.src + '/' + path.js +'/**/*.js'], { read: false }), {
-        addRootSlash: false,
-        transform: function(filePath, file, i, length) {
-          return '<script src="' + filePath.replace(path.src, '') + '"></script>';
-        }
-      }))
-
-    .pipe($.inject(
-      gulp.src([path.src + '/' + path.css +'/**/*.css'], { read: false }), {
-        addRootSlash: false,
-        transform: function(filePath, file, i, length) {
-          return '<link rel="stylesheet" href="' + filePath.replace(path.src, '') + '"/>';
-        }
-      }))
-    .pipe(gulp.dest(path.src + '/_layouts/'));
+// Copy xml and txt files to the "site" directory
+gulp.task('copy', function () {
+  return gulp.src([path.serve + '/*.txt', path.serve + '/*.xml'])
+    .pipe(gulp.dest(path.site))
+    .pipe($.size({ title: 'xml & txt' }))
 });
 
-// // Copy xml and txt files to the path.deploy directory
-// gulp.task('copy', function () {
-//   return gulp.src([path.src + '/*.txt', path.src + '/*.xml'])
-//     .pipe(gulp.dest(path.deploy))
-//     .pipe($.size({ title: 'xml & txt' }));
-// });
-
 // Optimizes all the CSS, HTML and concats the JS etc
-gulp.task('html', function () {
-  var assets = $.useref.assets({searchPath: path.deploy});
+gulp.task('html', ['styles'], function () {
+  var assets = $.useref.assets({searchPath: path.serve});
 
-  return gulp.src(path.deploy + '/**/*.html')
+  return gulp.src(path.serve + '/**/*.html')
     .pipe(assets)
     // Concatenate JavaScript files and preserve important comments
     .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
     // Minify CSS
     .pipe($.if('*.css', $.minifyCss()))
     // Start cache busting the files
-    .pipe($.revAll({ ignore: ['.eot', '.svg', '.ttf', '.woff', '.php'] }))
+    .pipe($.revAll({ ignore: ['.eot', '.svg', '.ttf', '.woff', '.woff2', '.jpg', '.png', '.php'] }))
     .pipe(assets.restore())
     // Conctenate your files based on what you specified in _layout/header.html
     .pipe($.useref())
     // Replace the asset names with their cache busted names
     .pipe($.revReplace())
     // Minify HTML
-    // .pipe($.if('*.html', $.htmlmin({
-    //   removeComments: true,
-    //   removeCommentsFromCDATA: true,
-    //   removeCDATASectionsFromCDATA: true,
-    //   collapseWhitespace: true,
-    //   collapseBooleanAttributes: true,
-    //   removeRedundantAttributes: true
-    // })))
+    .pipe($.if('*.html', $.htmlmin({
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      removeCDATASectionsFromCDATA: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeRedundantAttributes: true
+    })))
     // Send the output to the correct folder
-    .pipe(gulp.dest(path.deploy))
+    .pipe(gulp.dest(path.site))
     .pipe($.size({title: 'optimizations'}));
 });
-
-
 
 // Run JS Lint against your JS
 gulp.task('jslint', function () {
@@ -162,32 +139,15 @@ gulp.task('jslint', function () {
     .pipe($.jshint.reporter());
 });
 
-// Runs 'jekyll doctor' on your site to check for errors with your configuration
-// and will check for URL errors a well
-gulp.task('doctor', $.shell.task('jekyll doctor'));
-
-// BrowserSync will serve our site on a local server for us and other devices to use
-// It will also autoreload across all devices as well as keep the viewport synchronized
-// between them.
-gulp.task('serve:dev', ['jekyll:dev'], function () {
-  bs = browserSync({
-    notify: true,
-    // tunnel: '',
-    server: {
-      baseDir: path.serve
-    }
-  });
-});
-
 // Task to upload your site via Rsync to your server
-gulp.task('deploy', function () {
+gulp.task('rsync', function () {
   // Load in the variables needed for our Rsync synchronization
   var secret = require('./._rsync.json');
 
-  return gulp.src(path.deploy + '/**')
+  return gulp.src(path.site + '/**')
     .pipe($.rsync({
       // This uploads the contents of 'root', instead of the folder
-      root: path.deploy,
+      root: path.site,
       // Credentials ignored by repository
       hostname: secret.hostname,
       username: secret.username,
@@ -201,35 +161,43 @@ gulp.task('deploy', function () {
   }));
 });
 
-gulp.task('ghpages', function() {
-  return gulp.src('./'+path.deploy+'/**/*')
+// Task to upload your site to your personal GH Pages repo
+gulp.task('deploy', function () {
+  // Deploys your optimized site, you can change the settings in the html task if you want to
+  return gulp.src('./' + path.site + '/**/*')
     .pipe($.ghPages());
+});
+
+
+// Runs "jekyll doctor" on your site to check for errors with your configuration
+// and will check for URL errors a well
+gulp.task('doctor', $.shell.task('jekyll doctor'));
+
+// BrowserSync will serve our site on a local server for us and other devices to use
+// It will also autoreload across all devices as well as keep the viewport synchronized
+// between them.
+gulp.task('serve:dev', ['styles', 'jekyll:dev'], function () {
+  bs = browserSync({
+    notify: true,
+    // tunnel: '',
+    server: {
+      baseDir: path.serve
+    }
+  });
 });
 
 // These tasks will look for files that change while serving and will auto-regenerate or
 // reload the website accordingly. Update or add other files you need to be watched.
 gulp.task('watch', function () {
-  gulp.watch([path.src + '/**/*.md', path.src + '/**/*.html', path.src + '/**/*.xml', path.src + '/**/*.txt', path.src + '/**/*.js', path.src + '/**/*.scss', path.src + '/**/*.yml'], ['jekyll-rebuild']);
+  gulp.watch([path.src + '/**/*.md', path.src + '/**/*.yml', path.src + '/**/*.html', path.src + '/**/*.xml', path.src + '/**/*.txt', path.src + '/**/*.js'], ['jekyll-rebuild']);
   gulp.watch([path.serve + '/' + path.css + '/*.css'], reload);
-});
-
-
-gulp.task('connect-sync', function() {
-  php.server({}, function (){
-    browserSync({
-      proxy: 'localhost:8000'
-    });
-  });
-
-  gulp.watch('**/*.php').on('change', function () {
-    browserSync.reload();
-  });
+  gulp.watch([path.src + '/' + path.scss + '/**/*.scss'], ['styles']);
 });
 
 // Serve the site after optimizations to see that everything looks fine
 gulp.task('serve:prod', function () {
   php.server({
-    base: path.deploy,
+    base: path.site,
     open: true
   }, function (){
       bs = browserSync({
@@ -237,13 +205,13 @@ gulp.task('serve:prod', function () {
         notify: false,
         // tunnel: true,
         server: {
-          baseDir: path.deploy
+          baseDir: path.site
         }
     });
   });
 });
 
-// Default task, run when just writing 'gulp' in the terminal
+// Default task, run when just writing "gulp" in the terminal
 gulp.task('default', ['serve:dev', 'watch']);
 
 // Checks your CSS, JS and Jekyll for errors
@@ -252,10 +220,10 @@ gulp.task('check', ['jslint', 'doctor'], function () {
 });
 
 // Builds the site but doesn't serve it to you
-gulp.task('build', ['jekyll:prod'], function () {});
+gulp.task('build', ['jekyll:prod', 'styles'], function () {});
 
-// Builds your site with the 'build' command and then runs all the optimizations on
-// it and outputs it to './site'
+// Builds your site with the "build" command and then runs all the optimizations on
+// it and outputs it to "./site"
 gulp.task('publish', ['build'], function () {
-  gulp.start('html', 'images', 'fonts');
+  gulp.start('html', 'copy', 'images', 'fonts');
 });
