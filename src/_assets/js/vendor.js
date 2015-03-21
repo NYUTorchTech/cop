35199ad2a053704bff9f62e606b794efdd2b2e1b
Jannae Jacks <jannae@gmail.com>
2015-03-21 09:46:54 -0400

    First pass at jekyll-assets. WHY HAVE I NOT FOUND THIS BEFORE!?

---
+++ src/_assets/js/vendor.js
@@ -0,0 +1,6 @@
+/* Multi-line comment blocks (CSS, SCSS, JavaScript)
+ *= require vendor/jquery
+ *= require vendor/bootstrap
+ *= require vendor/jquery.easing
+ *= require vendor/jqBootstrapValidation
+ */
---
+++ src/_plugins/ext.rb
@@ -0,0 +1,5 @@
+require "jekyll-assets"
+require "jekyll-assets/bootstrap"
+
+# bootstrap requires minimum precision of 10, see https://github.com/twbs/bootstrap-sass/issues/409
+::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
